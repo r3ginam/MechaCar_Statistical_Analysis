609d4839e23e63d62e728be2f4095d7de69d77f2
Regina Manion <72927699+r3ginam@users.noreply.github.com>
2021-04-18 09:47:46 -0500

    added ttests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-# D 1: Linear Regression to Predict MPG
+# D1: Linear Regression to Predict MPG
 
 library(dplyr)
 
@@ -8,10 +8,18 @@ lm <- lm(formula= mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 
 summary(lm)
 
-# D 2: Visualization for Trip Analysis
+# D2: Visualization for Trip Analysis
 
 suspension_table <- read.csv(file='Suspension_Coil.csv', header= TRUE, sep=",", check.names= FALSE, stringsAsFactors = FALSE)
 
 total_summary <- suspension_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups= 'keep')
 
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups= 'keep')
+
+# D3: Technical Analysis
+
+t.test(suspension_table$PSI, mu=1500)
+
+t.test(subset(suspension_table, Manufacturing_Lot=='Lot1')$PSI, mu=1500)
+t.test(subset(suspension_table, Manufacturing_Lot=='Lot2')$PSI, mu=1500)
+t.test(subset(suspension_table, Manufacturing_Lot=='Lot3')$PSI, mu=1500)
