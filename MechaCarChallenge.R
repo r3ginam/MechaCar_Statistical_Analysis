aa111f0d63a4bf3ef55cf6c49e06a298e32dd64d
Regina Manion <72927699+r3ginam@users.noreply.github.com>
2021-04-18 09:13:44 -0500

    added viz for trip analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,11 @@ mpg_table <- read.csv(file='MechaCar_mpg.csv', header=TRUE, sep=",", check.names
 lm <- lm(formula= mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= mpg_table)
 
 summary(lm)
+
+# D 2: Visualization for Trip Analysis
+
+suspension_table <- read.csv(file='Suspension_Coil.csv', header= TRUE, sep=",", check.names= FALSE, stringsAsFactors = FALSE)
+
+total_summary <- suspension_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups= 'keep')
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups= 'keep')
